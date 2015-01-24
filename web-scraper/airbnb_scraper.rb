74462c8f5d246ad7959e2bfe8524115c759878ce
Dylan Wolff <dylanwolff@gmail.com>
2015-01-24 21:07:49 +1100

    Write scraped data to CSV

--- web-scraper/airbnb_scraper.rb
+++ web-scraper/airbnb_scraper.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require 'csv'
 
 # Store URL to be scraped
 url ="https://www.airbnb.com/s/Brooklyn--NY--United-States"
@@ -7,17 +8,35 @@ url ="https://www.airbnb.com/s/Brooklyn--NY--United-States"
 # Parse the page with Nokogiri
 page = Nokogiri::HTML(open(url))
 
-# Listing name
+# Store listing name in array
+name =[]
 page.css('div.h5.listing-name').each do |line|
-  puts line.text
+  name << line.text.strip
 end
 
-# Price
+# Store price in array
+price = []
 page.css('span.h3.price-amount').each do |line|
-  puts line.text
+  price << line.text
 end
 
-# Room type, number of reviews, location
+# Store details (room type, number of reviews, location) in array
+details = []
 page.css('div.text-muted.listing-location.text-truncate').each do |line|
-  puts line.text
+  details << line.text.strip.split(/ . /)
 end
+
+# Write data to CSV file
+CSV.open("airbnb_listings.csv", "w") do |file|
+  file << ["Listing Name", "Price", "Room Type", "Reviews", "Location"]
+
+  name.length.times do |i|
+    file << [name[i], price[i], details[i][0], details[i][1], details[i][2]]
+  end
+end
+
+
+
+
+
+
