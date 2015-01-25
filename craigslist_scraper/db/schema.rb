bb5362d473b3dcfa7eeee171b68846bbd07479a8
Dylan Wolff <dylanwolff@gmail.com>
2015-01-26 10:02:20 +1100

    Add loop to scrape all new data

--- craigslist_scraper/db/schema.rb
+++ craigslist_scraper/db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150125062443) do
+ActiveRecord::Schema.define(version: 20150125225137) do
+
+  create_table "anchors", force: :cascade do |t|
+    t.string   "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "images", force: :cascade do |t|
     t.text     "url"
