8226716580d68e6de8bd313f0ac29162c3aab473
Dylan Wolff <dylanwolff@gmail.com>
2015-01-26 15:04:22 +1100

    Remove web-scraper

--- web_scraper/wikipedia_scraper.rb
+++
@@ -1,9 +0,0 @@
-require 'open-uri'
-require 'nokogiri'
-
-url = "http://en.wikipedia.org/wiki/List_of_current_NBA_team_rosters"
-page = Nokogiri::HTML(open(url))
-
-page.css('td[style="text-align:left;"]').each do |line|
-  puts line.text
-end
