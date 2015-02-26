603ac68e554b1012e22e6f1457e3889eb34d830a
Dylan Wolff <dylanwolff@gmail.com>
2015-02-27 09:35:30 +1100

    Add ActiveRecord rescue, redirect unknown pages to root

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -76,6 +76,8 @@ class PostsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        redirect_to root_url
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
